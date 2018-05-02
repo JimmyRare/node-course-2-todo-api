e487faefb57a295758283cd0548310c1442376b9
Jimmy Engström <jimmy.engstrom@ntm.eu>
2018-05-02 16:52:00 +0200

    Add tests for POST /users/login

--- server/server.js
+++ server/server.js
@@ -119,10 +119,6 @@ app.get('/users/me', authenticate, (req, res) => {
     res.send(req.user);
 });
 
-app.listen(port, () => {
-  console.log(`Started up at port ${port}`);
-});
-
 app.post('/users/login', (req, res) => {
     var body = _.pick(req.body, ['email', 'password']);
 
@@ -136,5 +132,11 @@ app.post('/users/login', (req, res) => {
             res.status(400).send();
         });
 });
+
+app.listen(port, () => {
+  console.log(`Started up at port ${port}`);
+});
+
+
 
 module.exports = {app};
